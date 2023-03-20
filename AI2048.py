661c0bdcc412d60f25ea943e2925f4aad258597e
Willdew <william.brochner@gmail.com~>
2023-03-20 14:15:18 +0100

    Added Heuristic function

--- AI2048.py
+++ AI2048.py
@@ -13,6 +13,32 @@ class AI:
         self.max_depth = 1
         self.best_move = None
 
+    def evaluate_board(self, board):
+        score = 0
+
+        # Count the number of empty cells on the board
+        num_empty = np.sum(board == 0)
+
+        # Score for tiles in same row or column with the same number
+        same_row_score = np.sum(np.max(board[:-1, :] == board[1:, :], axis=1))
+        same_col_score = np.sum(np.max(board[:, :-1] == board[:, 1:], axis=0))
+        score += (same_row_score + same_col_score) * 5
+
+        # Score for order of numbers on board
+        sorted_board = np.sort(board, axis=None)[::-1].reshape((4, 4))
+        ordered_score = 0
+        if sorted_board[0, 3] == np.max(board):
+            ordered_score += 10
+        for i in range(3):
+            for j in range(3):
+                if sorted_board[i, j] > sorted_board[i, j + 1] and sorted_board[i + 1, j] > sorted_board[i, j]:
+                    ordered_score += 1
+        score += ordered_score
+
+        # Penalty for number of empty cells on board
+        score += num_empty * 2
+
+        return score
 
     def maximize(self, depth, board):
         moves = ['w', 'a', 's', 'd']
