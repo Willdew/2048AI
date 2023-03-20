da90134cd03552752f05c0af9744b9f3144776a7
Bajers <augustweijers@gmail.com>
2023-03-20 12:39:11 +0100

    fucker lige rund

---
+++ 2048AI.py
@@ -0,0 +1,22 @@
+import numpy as np
+from .2048game.py import *
+
+
+
+
+class AI:
+
+    def __init__(self, board):
+        self.board = board
+        self.score = 0
+        self.depth = 0
+        self.max_depth = 1
+        self.best_move = None
+
+    def add_tile(self):
+        nowBoardR = update_board(self.board, 'r')
+        nowBoardL = update_board(self.board, 'l')
+        nowBoardU = update_board(self.board, 'u')
+        nowBoardD = update_board(self.board, 'd')
+        for i in range(4):
+            for j in range()
